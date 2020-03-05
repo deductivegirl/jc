a00fb432638b2d8a5dd45a250456889a5ee8fbc8
Jessica <deductivegirl@gmail.com>
2020-03-05 09:57:14 -0700

    fixed loop error I made

--- star_wars/star_wars.js
+++ star_wars/star_wars.js
@@ -5,15 +5,17 @@ import { species } from './species.js'
 import { starships } from './starships.js'
 import { vehicles } from './vehicles.js'
 
+
+// People
 let peopleName = document.querySelector('#pName')
 
 let counter = 1
 
 let castList = document.createElement("ul")
 people.forEach(person => {
-   let castItem = document.createElement("li")
+   /*let castItem = document.createElement("li")
    castItem.textContent = person.name
-   castList.appendChild(castItem)
+   castList.appendChild(castItem)*/
 
    let personAnchor = document.createElement("a")
    personAnchor.href = "#"
@@ -22,17 +24,24 @@ people.forEach(person => {
    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
 
    personImg.addEventListener('error', (event) => {
+      //Placeholder Image
       //personImg.src = '../images/starwarspic.jpg'
       personImg.hidden = true
    })
-
-   personImg.addEventListener("click", function (event) {
-      console.log("hi")
-   })
-
+   
    personAnchor.appendChild(personImg)
    peopleName.appendChild(personAnchor)
    counter++
+
+   personImg.addEventListener("click", (event) => {
+         let personHead = document.createElement("h3")
+         personHead.textContent = person.name
+         peopleName.appendChild(personHead)
+   })
 })
 
-peopleName.appendChild(castList)
+
+
+
+//Lists names of people
+//peopleName.appendChild(castList)
