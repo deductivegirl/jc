47cd1390a510c2ecf942b33fc5be2d493509cc29
Jessica <deductivegirl@gmail.com>
2020-03-31 10:41:46 -0600

    congress improvements

--- congress/congress.js
+++ congress/congress.js
@@ -6,3 +6,26 @@ const container = document.querySelector('.container')
 const filterSenators = (prop, value => {
     return senators.filter(senator => senator[prop] === value)
 })
+
+function populateContainer() {
+    senators.forEach(senator => {
+        let middleName = senator.middle_name ? ` ${senator.middle_name}` : ` `
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+
+        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
+
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        container.appendChild(senFigure)
+    })
+}
+
+populateContainer()
+
+console.log(filterSenators('party', 'R'))
+console.log(filterSenators('party', 'D'))
+console.log(filterSenators('party', 'ID'))
