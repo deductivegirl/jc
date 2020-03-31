2f5245b4e7a44251501e0afc8befb12e320457cc
Jessica <deductivegirl@gmail.com>
2020-03-31 10:58:58 -0600

    mapping

--- congress/congress.js
+++ congress/congress.js
@@ -7,16 +7,25 @@ const filterSenators = (prop, value => {
     return senators.filter(senator => senator[prop] === value)
 })
 
-function populateContainer() {
-    senators.forEach(senator => {
-        let middleName = senator.middle_name ? ` ${senator.middle_name}` : ` `
+const mappedSenators = senators.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name}` : ` `
+    return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+        
+    }
+})
+
+function populateContainer(smallSenatorsArray) {
+    console.log(smallSenatorsArray)
+    smallSenatorsArray.forEach(senator => {
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
 
-        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-
-        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
 
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
@@ -24,8 +33,4 @@ function populateContainer() {
     })
 }
 
-populateContainer()
-
-console.log(filterSenators('party', 'R'))
-console.log(filterSenators('party', 'D'))
-console.log(filterSenators('party', 'ID'))
+populateContainer(mappedSenators)
