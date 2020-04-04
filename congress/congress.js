863dffb41b23ce4bb773b547cc56ca4664970b8a
Jessica <deductivegirl@gmail.com>
2020-04-03 22:19:38 -0600

    congress fixes

--- congress/congress.js
+++ congress/congress.js
@@ -9,19 +9,20 @@ const filterSenators = (prop, value) => {
     return senators.filter(senator => senator[prop] === value)
 }
 
-const mappedSenators = senators.map(senator => {
-    let middleName = senator.middle_name ? ` ${senator.middle_name}` : ` `
-    return {
-        id: senator.id,
-        name: `${senator.first_name}${middleName}${senator.last_name}`,
-        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-        
-    }
-})
+function simpMapSenators(senatorArray) { 
+    return senatorArray.map(senator => {
+        let middleName = senator.middle_name ? ` ${senator.middle_name}` : ` `
+        return {
+            id: senator.id,
+            name: `${senator.first_name}${middleName} ${senator.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+            seniority: parseInt(senator.seniority, 10),
+        }
+    })
+}
 
 function populateContainer(smallSenatorsArray) {
-    console.log(smallSenatorsArray)
-    smallSenatorsArray.forEach(senator => {
+    return smallSenatorsArray.forEach(senator => {
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
@@ -35,4 +36,17 @@ function populateContainer(smallSenatorsArray) {
     })
 }
 
-populateContainer(mappedSenators)
+//filterSenators(what you're checking, what you're looking for)
+const republicans = filterSenators('party', 'R')
+const democrats = filterSenators('party', 'D')
+const independents = filterSenators('party', 'ID')
+
+const mostSeniority = simpMapSenators(republicans).reduce(
+    (acc, senator) => {
+        return acc.seniority > senator.seniority ? acc : senator
+    }, {}
+)
+
+console.log(mostSeniority)
+
+populateContainer(simpMapSenators(republicans))
