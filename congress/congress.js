dff159d481ae9ca9996c0b3c8d81b6db1406ec68
Jessica <deductivegirl@gmail.com>
2020-04-08 23:29:00 -0600

    Congress outlines

--- congress/congress.js
+++ congress/congress.js
@@ -17,6 +17,9 @@ function simpMapSenators(senatorArray) {
             name: `${senator.first_name}${middleName} ${senator.last_name}`,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
             seniority: parseInt(senator.seniority, 10),
+            votesWithPartyPct: senator.votes_with_party_pct,
+            party: senator.party,
+            missed_votes_pct: senator.missed_votes_pct,
         }
     })
 }
@@ -41,12 +44,34 @@ const republicans = filterSenators('party', 'R')
 const democrats = filterSenators('party', 'D')
 const independents = filterSenators('party', 'ID')
 
-const mostSeniority = simpMapSenators(republicans).reduce(
+const mostSeniority = simpMapSenators(democrats).reduce(
     (acc, senator) => {
         return acc.seniority > senator.seniority ? acc : senator
-    }, {}
+    }
 )
 
+const mostMissedVotes = simpMapSenators(senators).reduce((acc, senator) => {
+    return acc.missed_votes_pct > senator.missed_votes_pct ? acc : senator
+})
+
+/*const loyalArray = simpMapSenators(senators).map(senator => {
+    if (senator.votesWithPartyPct === 100) {
+        return senator
+    }
+})*/
+
+let loyalArray = []
+
+const mostLoyal = simpMapSenators(senators).reduce((acc, senator) => {
+    if (senator.votesWithPartyPct === 100) {
+        loyalArray.push(senator)
+    }
+    return acc.votesWithPartyPct > senator.votesWithPartyPct ? acc : senator
+})
+
 console.log(mostSeniority)
+console.log(loyalArray)
+console.log(mostMissedVotes)
+
 
 populateContainer(simpMapSenators(republicans))
