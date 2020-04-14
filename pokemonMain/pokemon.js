c85205eebe6e38499040fe8704894ed5e2b79093
Jessica <deductivegirl@gmail.com>
2020-04-14 10:17:39 -0600

    added first 25 images

--- pokemonMain/pokemon.js
+++ pokemonMain/pokemon.js
@@ -1,5 +1,9 @@
 let poketainer = document.querySelector('.poketainer')
+let startButton = document.querySelector('#startButton')
 
+startButton.addEventListener('click', () => {
+    loadPage()
+})
 // below is too specific
 /* function getPokedata(url) {
     fetch(url).then(function (response) {
@@ -25,17 +29,19 @@ async function getAPIData(url) {
     }
 }
 
-getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
-    (data) => {
-        for (const pokemon of data.results) {
-            getAPIData(pokemon.url).then(
-                (pokeData) => {
-                    populatePokecard(pokeData)
-                }
-            )
+function loadPage() {
+    getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+        (data) => {
+            for (const pokemon of data.results) {
+                getAPIData(pokemon.url).then(
+                    (pokeData) => {
+                        populatePokecard(pokeData)
+                    }
+                )
+            }
         }
-    }
-)
+    )
+}
 
 function populatePokecard(onePokemon) {
         let pokeScene = document.createElement('div')
